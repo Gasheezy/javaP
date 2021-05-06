9c482e781607bfc518af3b6eded60b053274580d
Gasheezy <mungai.gachango@gmail.com>
2021-05-06 10:11:56 +0300

    TillExercise3

--- BasicExercises1.java
+++ BasicExercises1.java
@@ -19,5 +19,11 @@ class BasicExercises1 {
         //divide two numbers and print on the screen
         Scanner input = new Scanner(System.in);
         System.out.println("Input the first number");
+        int num1 = input.nextInt();
+        System.out.println("Input the second number");
+        int num2 = input.nextInt();
+
+        int div = num1/num2;
+        System.out.println("Dividing the two numbers gives: " +div);
     }
 }
